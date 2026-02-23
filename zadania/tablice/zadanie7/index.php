b4914f4617568429f2d19ca786008f0230ffce63
Igor <151391534+i120ms@users.noreply.github.com>
2026-02-23 08:30:14 +0100

    Update index.php

--- zadania/tablice/zadanie7/index.php
+++ zadania/tablice/zadanie7/index.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table>
+        <tr>
+            <th></th>
+            <th>Kol1</th>
+            <th>Kol2</th>
+            <th>Kol3</th>
+            <th>Kol4</th>
+            <th>Kol5</th>
+        </tr>
 
+        <?php
+            for($i=0; $i<5; $i++){
+                echo "<tr>";
+                    echo "<th>";
+                    echo "Wiersz";
+                    echo $i+1;
+                    echo "</th>";
+
+                for($j=0; $j<5;$j++){
+                    $tab[$i][$j] = rand(10,99);
+                    echo "<td>";
+                        echo $tab[$i][$j];
+                    echo "</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
