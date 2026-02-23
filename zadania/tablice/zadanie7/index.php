d74ec4ea802664480ef86e2f1f6ea65c77a914c4
Igor <151391534+i120ms@users.noreply.github.com>
2026-02-23 09:02:42 +0100

    Update index.php

--- zadania/tablice/zadanie7/index.php
+++ zadania/tablice/zadanie7/index.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>Tablice dwuwymiarowe - Zadanie</h1>
+    <h3>Wylosowane liczby</h3>
+
     <table>
         <tr>
             <th></th>
@@ -18,14 +21,15 @@
         </tr>
 
         <?php
-            for($i=0; $i<5; $i++){
+        $iloscW = 5;
+            for($i=0; $i<$iloscW; $i++){
                 echo "<tr>";
                     echo "<th>";
                     echo "Wiersz";
                     echo $i+1;
                     echo "</th>";
 
-                for($j=0; $j<5;$j++){
+            for($j=0; $j<5;$j++){
                     $tab[$i][$j] = rand(10,99);
                     echo "<td>";
                         echo $tab[$i][$j];
