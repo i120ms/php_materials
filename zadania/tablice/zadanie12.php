7bcac971ee330e8f3913d4cc7882077d0c4415dc
Igor <151391534+i120ms@users.noreply.github.com>
2026-03-02 09:35:55 +0100

    Create zadanie12.php

---
+++ zadania/tablice/zadanie12.php
@@ -0,0 +1,15 @@
+<?php
+$uczen1 = [
+    "imie" => "Anna",
+    "nazwisko" => "Kowalska",
+    "klasa" => "1a"
+];
+
+$uczen2 = [
+    "imie" => "Marcin",
+    "nazwisko" => "Niger",
+    "klasa" => "4d"
+];
+
+// itd
+?>
