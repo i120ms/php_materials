a5ead6d255e16f1acb6f32569c8fc1466e032651
Igor <151391534+i120ms@users.noreply.github.com>
2026-03-26 08:55:12 +0100

    Update 1.php

--- INF.03/Egzamin_Jonczyk/1.php
+++ INF.03/Egzamin_Jonczyk/1.php
@@ -6,14 +6,26 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>Kup dziś samochód!</h3>
+    <img src="samochod.png">
     <?php
     // połowa punktów na egzaminie za samo $conn echo mysqli_close i echo "";
     // 3/4 za dodatkowo $query i $result
     $conn = mysqli_connect("localhost", "root", "", "auto");
     $query = "SELECT * FROM Samochody where marka = 'Toyota'";
     $result = mysqli_query($conn, $query);
+
+    echo "<ul>";
+    while($row = mysqli_fetch_array($result))
+    echo "<li>".$row['marka']." ".$row[3].'</li>';
+    echo "</ul>";
+/*
     $row = mysqli_fetch_array($result);
-    echo $row[marka]." ".$row[3];
+    echo $row['marka']." ".$row[3].'<br>';
+
+    $row = mysqli_fetch_array($result);
+    echo $row['marka']." ".$row[3].'<br>';
+*/
 
     mysql_close($conn);
 
